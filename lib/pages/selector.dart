0813f2e4c6eac40deefb11189625f60b54926c8e
jaime <jachevalier@bgeneral.com>
2020-03-15 20:00:50 -0500

    Se agrega separador

--- lib/pages/selector.dart
+++ lib/pages/selector.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_bar/widget/month.dart';
 
 class Selector extends StatelessWidget {
+
+  /*PageController _pageController;
+  void initState(){
+    _pageController = PageController(
+      initialPage: 9,
+      viewportFraction: 0.3,
+    );
+  }*/
+   
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "Selector"
+    return SizedBox.fromSize(
+      size: Size.fromHeight(70.0),
+      child: PageView(
+        //controller: _pageController,
+        children: <Widget>[
+          MonthText(month: "Enero"),
+          MonthText(month: "Febrero"),
+          MonthText(month: "Marzo"),
+          MonthText(month: "Abril"),
+          MonthText(month: "Mayo"),
+          MonthText(month: "Junio"),
+          MonthText(month: "Julio"),
+          MonthText(month: "Agosto"),
+          MonthText(month: "Septiembre"),
+          MonthText(month: "Octubre"),
+          MonthText(month: "Noviembre"),
+          MonthText(month: "Diciembre"),
+        ],
+      )
     );
   }
 } 
